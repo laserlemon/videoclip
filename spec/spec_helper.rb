9ff8dad1b5158c87912007fa35706aedd7e023f9
Steve Richert <steve.richert@gmail.com>
2012-02-16 17:03:59 -0500

    Test the logic behind choosing a matching implementation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,9 @@ require "active_record"
 require "videoclip"
 
 Dir[File.expand_path("../support/**/*.rb", __FILE__)].each{|f| require f }
+
+RSpec.configure do |config|
+  config.before do
+    Videoclip::Video.implementations.clear
+  end
+end
--- spec/videoclip/video_spec.rb
+++ spec/videoclip/video_spec.rb
@@ -24,9 +24,45 @@ describe Videoclip::Video do
       uri.to_s.should == url
     end
 
-    it "bombs on bad URLs" do
+    it "bombs on invalid URLs" do
       url = "youtube.com/watch?v=qybUFnY7Y8w"
       expect{ Videoclip::Video.parse(url) }.to raise_error(Videoclip::InvalidUrl)
+    end
+  end
+
+  context ".match" do
+    def match
+      url = "http://www.youtube.com/watch?v=qybUFnY7Y8w"
+      uri = Videoclip::Video.parse(url)
+      Videoclip::Video.match(uri)
+    end
+
+    it "calls .matches? on implementations" do
+      camera_1, camera_2 = mock, mock
+      Videoclip::Video.implementations.replace([camera_1, camera_2])
+      camera_1.should_receive(:matches?).once
+      camera_2.should_receive(:matches?).once
+      match rescue nil
+    end
+
+    it "returns the first implementation that matches" do
+      camera_1, camera_2 = mock(:matches? => true), mock(:matches? => false)
+      Videoclip::Video.implementations.replace([camera_1, camera_2])
+      match.should == camera_1
+
+      camera_1, camera_2 = mock(:matches? => false), mock(:matches? => true)
+      Videoclip::Video.implementations.replace([camera_1, camera_2])
+      match.should == camera_2
+
+      camera_1, camera_2 = mock(:matches? => true), mock(:matches? => true)
+      Videoclip::Video.implementations.replace([camera_1, camera_2])
+      match.should == camera_1
+    end
+
+    it "bombs if no implementations match" do
+      camera_1, camera_2 = mock(:matches? => false), mock(:matches? => false)
+      Videoclip::Video.implementations.replace([camera_1, camera_2])
+      expect{ match }.to raise_error(Videoclip::UnrecognizedUrl)
     end
   end
 end
