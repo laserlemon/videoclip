5541d5428ccb8173b1a244fb5d405d16e2864efe
Steve Richert <steve.richert@gmail.com>
2012-02-16 12:21:34 -0500

    Pass the video class inheritance test

--- spec/video_spec.rb
+++ spec/video_spec.rb
@@ -1,5 +1,9 @@
 require "spec_helper"
 
 describe Videoclip::Video do
-  it "keeps track of its inherited video classes"
+  it "keeps track of its inherited video classes" do
+    Videoclip::Video.implementations.should == []
+    class CustomVideo < Videoclip::Video; end
+    Videoclip::Video.implementations.should == [CustomVideo]
+  end
 end
